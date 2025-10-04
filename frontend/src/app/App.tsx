2f09590b7ea8cbeb9324f380ce0dc78def5fef8a
dylanzeballos <zeballosperedodylanamin@gmail.com>
2025-10-04 00:02:33 -0400

    fix: router remove suspense from imports

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,15 +1,14 @@
-import { RouterProvider } from 'react-router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { Toaster } from 'sonner'
-import AppRouter from './AppRouter';
+import{ AppRouterProvider } from './AppRouter';
 
 const queryClient = new QueryClient()
 
 function App() {
     return (
         <QueryClientProvider client={queryClient}>
-            <RouterProvider router={AppRouter} />
+            <AppRouterProvider />
             <Toaster
                 position="top-right"
                 richColors
--- frontend/src/app/AppRouter.tsx
+++ frontend/src/app/AppRouter.tsx
@@ -1,66 +1,27 @@
-import { createBrowserRouter } from 'react-router';
+import { createBrowserRouter, RouterProvider } from 'react-router';
 import MainLayout from '@/layout/MainLayout';
+import { SignInPage } from '@/features/auth/pages/SignInPage';
+import { SignUpPage } from '@/features/auth/pages/SignUpPage';
 
 const AppRouter = createBrowserRouter([
   {
     path: '/',
-    Component: MainLayout,
-    /* children: [
-      {
-        index: true,
-        lazy: () =>
-          import('../features/landing/presentation/pages/LandingPage'),
-      },
-      {
-        path: 'dashboard',
-        lazy: () => import('../features/dashboard/presentation/pages/HomePage'),
-      },
-      {
-        path: 'auth',
-        children: [
-          {
-            path: 'login',
-            lazy: () => import('../features/auth/presentation/pages/LoginPage'),
-          },
-          {
-            path: 'register',
-            lazy: () =>
-              import('../features/auth/presentation/pages/RegisterPage'),
-          },
-        ],
-      },
-      {
-        path: 'courses',
-        children: [
-          {
-            index: true,
-            lazy: () =>
-              import('../features/courses/presentation/pages/CoursesPage'),
-          },
-          {
-            path: ':id',
-            lazy: () =>
-              import('../features/courses/presentation/pages/CourseDetailPage'),
-          },
-        ],
-      },
-      {
-        path: 'profile',
-        children: [
-          {
-            index: true,
-            lazy: () =>
-              import('../features/users/presentation/pages/ProfilePage'),
-          },
-          {
-            path: 'edit',
-            lazy: () =>
-              import('../features/users/presentation/pages/EditProfilePage'),
-          },
-        ],
-      },
-    ], */
+    element: <MainLayout />,
+  },
+  {
+    path: 'login',
+    element: <SignInPage />,
+  },
+  {
+    path: 'register',
+    element: <SignUpPage />
   },
 ]);
 
+export const AppRouterProvider = () => {
+  return <RouterProvider router={AppRouter} />;
+};
+
 export default AppRouter;
+
+
