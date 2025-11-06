ba5e4bd013878219703cb15b1484dae0775fc868
dylanzeballos <zeballosperedodylanamin@gmail.com>
2025-11-05 22:39:21 -0400

    fix: type in curren user decorator

--- backend/src/auth/infrastructure/decorators/current-user.decorator.ts
+++ backend/src/auth/infrastructure/decorators/current-user.decorator.ts
@@ -2,12 +2,12 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { User } from '../../domain/entities/user.entity';
 
 export const CurrentUser = createParamDecorator(
-  (data: string | undefined, ctx: ExecutionContext): User | any => {
+  (data: string | undefined, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest<{ user: User }>();
     const user = request.user;
 
     if (data && user) {
-      return user[data as keyof User] as any;
+      return user[data as keyof User];
     }
 
     return user;
